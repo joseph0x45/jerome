3d62217587ba069831d188cc047606e22807a26a
joseph0x45 <zozozozeph@gmail.com>
2024-11-05 23:33:52 +0000

    assign roles to user and remove them upon reacting to them

--- main.go
+++ main.go
@@ -17,6 +17,9 @@ var defaultRoleMentionable bool = true
 
 const prefix = "!jerome"
 
+const rolesMessageID = "1303494297357783092"
+const rolesChannelID = "1302744612049387540"
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		panic(err)
@@ -38,17 +41,14 @@ func main() {
 		if partsLen >= 2 {
 			cmd := parts[1]
 			if cmd == "create_role" {
-				log.Println("1")
 				member, err := s.GuildMember(m.GuildID, m.Author.ID)
 				if err != nil {
-					log.Println("2")
 					s.ChannelMessageSend(m.ChannelID, "Something went wrong")
 					log.Println("Error while getting guild member:", err.Error())
 					return
 				}
 				isAuthorized := false
 				for _, roleID := range member.Roles {
-					log.Println("3")
 					role, err := s.State.Role(m.GuildID, roleID)
 					if err != nil {
 						s.ChannelMessageSend(m.ChannelID, "Something went wrong")
@@ -61,19 +61,16 @@ func main() {
 					}
 				}
 				if !isAuthorized {
-					log.Println("4")
 					s.ChannelMessageSend(m.ChannelID, "You are not authorized to do this action bozo")
 					return
 				}
 				if partsLen != 3 {
-					log.Println("5")
 					s.ChannelMessageSend(
 						m.ChannelID,
 						"Incorrect usage of command 'create_role'\n\nUsage: @jerome create_role <role_name>",
 					)
 					return
 				}
-				log.Println("6")
 				roleName := parts[2]
 				_, err = s.GuildRoleCreate(m.GuildID, &discordgo.RoleParams{
 					Name:        roleName,
@@ -81,16 +78,14 @@ func main() {
 					Hoist:       &defaultRolesHoist,
 					Mentionable: &defaultRoleMentionable,
 				})
-				log.Println("7")
 				if err != nil {
-					log.Println("8")
 					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Failed to create role %s. Cause: %s :)", roleName, err.Error()))
 					return
 				}
-				log.Println("9")
 				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Role %s created :)", roleName))
 				return
 			}
+			// List all roles
 			if cmd == "list_roles" {
 				roles, err := s.GuildRoles(m.GuildID)
 				if err != nil {
@@ -108,11 +103,95 @@ func main() {
 				s.ChannelMessageSend(m.ChannelID, message)
 				return
 			}
+			//Setup the reactions
+			if cmd == "setup_reactions" {
+				message, err := s.ChannelMessage(rolesChannelID, rolesMessageID)
+				if err != nil {
+					log.Println("Error while getting message:", err.Error())
+					s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+					return
+				}
+				reactionsMap := map[string]bool{}
+				for _, reaction := range message.Reactions {
+					reactionsMap[reaction.Emoji.ID] = true
+				}
+				emojis, err := s.GuildEmojis(m.GuildID)
+				if err != nil {
+					log.Println("Error while getting all custom emojis:", err.Error())
+					s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+					return
+				}
+				for _, emoji := range emojis {
+					if reactionsMap[emoji.ID] {
+						log.Println("Already reacted with this emoji. Skipping")
+						continue
+					}
+					err = s.MessageReactionAdd(rolesChannelID, rolesMessageID, fmt.Sprintf("%s:%s", emoji.Name, emoji.ID))
+					if err != nil {
+						log.Println("Error while reacting to message:", err.Error())
+					}
+				}
+				return
+			}
 		}
 	})
+
 	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
-		fmt.Printf("%+v", m)
+		roles, err := s.GuildRoles(m.GuildID)
+		if err != nil {
+			log.Println("Error while getting all roles:", err.Error())
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+			return
+		}
+		roleID := ""
+		for _, role := range roles {
+			if role.Name == m.Emoji.Name {
+				log.Println("Found role:", m.Emoji.Name)
+				roleID = role.ID
+				break
+			}
+		}
+		if roleID == "" {
+			log.Println("Role ", m.Emoji.Name, "not found")
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+			return
+		}
+		err = s.GuildMemberRoleAdd(m.GuildID, m.UserID, roleID)
+		if err != nil {
+			log.Println("Error while adding role to user:", err.Error())
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+			return
+		}
 	})
+
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageReactionRemove) {
+		roles, err := s.GuildRoles(m.GuildID)
+		if err != nil {
+			log.Println("Error while getting all roles:", err.Error())
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+			return
+		}
+		roleID := ""
+		for _, role := range roles {
+			if role.Name == m.Emoji.Name {
+				log.Println("Found role:", m.Emoji.Name)
+				roleID = role.ID
+				break
+			}
+		}
+		if roleID == "" {
+			log.Println("Role ", m.Emoji.Name, "not found")
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+			return
+		}
+		err = s.GuildMemberRoleRemove(m.GuildID, m.UserID, roleID)
+		if err != nil {
+			log.Println("Error while removing role from user:", err.Error())
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong")
+			return
+		}
+	})
+
 	session.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 	err = session.Open()
 	if err != nil {
